7b0b72e24a474cd17cf503cabdc50987f7b472f5
andreaekey <aekey@broadinstitute.org>
2016-03-22 17:46:32 -0400

    summary_stats argument assertions
    rearranged/added assertions to precede stats calculation

--- summary_stats.m
+++ summary_stats.m
@@ -1,20 +1,27 @@
 function smry = summary_stats(s, varargin)
+% SUMMARY_STATS computes summary stats on (numeric) fields of a scalar struct
+% Note: If struct is not scalar, try converting to scalar: toscalarstruct(s)
+    
+    % check arguments
     narginchk(1,2);
     assert(isstruct(s) && isscalar(s), 'Input must be scalar struct');
+    isnum = structfun(@isnumeric,s);
+    assert(all(isnum), 'Fields must be numeric');
+    
+    validstats = {'n','min','max','mean','median','std','iqr','mad','aad','cv','robcv','fivenum','fivenum_p','nan'};
+    if nargin == 2
+        statlist = varargin{1};
+    else
+        statlist = {'min','max','mean','median','std','iqr','mad','cv'};
+    end
+    assert(all(ismember(statlist,validstats)),'Invalid statistic input(s)');
+    
     
     % compute stats
     desc = structfun(@describe,s,'unif',false);
     names = fieldnames(desc);
-    % get
-    if nargin == 2
-        statlist = varargin{1};
-    else
-        statlist = {'min','max','mean','median','std','iqr','mad','cv','fivenum'};
-    end
-    
-    validstats = {'n','min','max','mean','median','std','iqr','mad','aad','cv','robcv','fivenum','fivenum_p','nan'};
-    assert(all(ismember(statlist,validstats)),'Invalid statistic input');
-    
-    smry = structfun(@(x) keepfield(x,statlist),desc);
+
     [smry.names] = names{:};
+    smry = structfun(@(x) keepfield(x,statlist),desc);
+   
 end
